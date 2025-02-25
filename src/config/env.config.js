b23c244fca59f95ed5b10416a12dd10412de1113
NoeOnDev <alxg5516@gmail.com>
2025-02-25 02:33:52 -0600

    Agregar configuración de CORS y soporte para envío de correos electrónicos en la configuración del entorno

--- src/config/env.config.js
+++ src/config/env.config.js
@@ -14,4 +14,14 @@ export const envConfig = {
     accessExpiration: process.env.JWT_ACCESS_EXPIRES,
     refreshExpiration: process.env.JWT_REFRESH_EXPIRES,
   },
+  client: {
+    url: process.env.CLIENT_URL,
+  },
+  email: {
+    host: process.env.EMAIL_HOST,
+    port: process.env.EMAIL_PORT,
+    secure: process.env.EMAIL_SECURE === "true",
+    user: process.env.EMAIL_USER,
+    password: process.env.EMAIL_PASS,
+  },
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 import { envConfig } from "./config/env.config.js";
 import connectWithRetry from "./database/connection.js";
 import { DatabaseInitializer } from "./database/init.js";
@@ -7,6 +8,7 @@ import { createUserRouter } from "./routes/user.routes.js";
 const app = express();
 const port = envConfig.port;
 
+app.use(cors());
 app.use(express.json());
 
 const startServer = async () => {
